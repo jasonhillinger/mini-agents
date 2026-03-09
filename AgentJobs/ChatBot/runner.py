045f014929dc2450641ed69722d3ec5c2158541e
Jason <jasonhillinger@gmail.com>
2026-03-08 20:22:18 -0400

    Moved chatbots name and systemPrompt to its own yaml file

--- AgentJobs/ChatBot/runner.py
+++ AgentJobs/ChatBot/runner.py
@@ -1,13 +1,18 @@
 from Agents.AIAgent import AIAgent
 from LLM.LLM import LLM
+from Utils.yamlUtils import getConfigFromYaml
 
 
 def run() -> None:
+    config = getConfigFromYaml("chatBot.yaml")
+    name = config["agent"]["name"]
+    systemPrompt = config["agent"]["systemPrompt"]
+
     llm = LLM.factory()
     agent = AIAgent(
-        name="PHP Expert",
+        name=name,
         llm=llm,
-        systemPrompt="You are a backend software developer who expertise in PHP",
+        systemPrompt=systemPrompt,
     )
 
     while True:
